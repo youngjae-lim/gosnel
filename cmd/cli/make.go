80d98193bcce429f749640c145e01eea148560bc
Youngjae Lim <limyoungjae@gmail.com>
2021-11-10 23:40:14 -0500

    Started worn on 'make migration' in the CLI application

---
+++ cmd/cli/make.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+func doMake(arg2, arg3 string) error {
+	switch arg2 {
+	case "migration":
+		dbType := gos.DB.DbType
+		if arg3 = "" {
+			exitGracefully(errors.New("you must give the migration name"))
+		}
+
+		fileName := fmt.Sprintf("%d_%s", time.Now().UnixMicro(), arg3)
+
+		upFile := gos.RootPath + "/migrations/" + fileName + "." + dbType + ".up.sql"
+		downFile := gos.RootPath + "/migrations/" + fileName + "." + dbType + ".down.sql"
+	}
+	return nil
+}
