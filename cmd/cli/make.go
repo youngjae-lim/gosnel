5a9893f4e92a995cf0422576957c56ec77be09b8
Youngjae Lim <limyoungjae@gmail.com>
2021-11-11 22:40:43 -0500

    Added sql templates for db migration

--- cmd/cli/make.go
+++ cmd/cli/make.go
@@ -10,7 +10,7 @@ func doMake(arg2, arg3 string) error {
 	switch arg2 {
 	case "migration":
 		dbType := gos.DB.DbType
-		if arg3 = "" {
+		if arg3 == "" {
 			exitGracefully(errors.New("you must give the migration name"))
 		}
 
@@ -18,6 +18,16 @@ func doMake(arg2, arg3 string) error {
 
 		upFile := gos.RootPath + "/migrations/" + fileName + "." + dbType + ".up.sql"
 		downFile := gos.RootPath + "/migrations/" + fileName + "." + dbType + ".down.sql"
+
+		err := copyFileFromTemplate("templates/migrations/migration." + dbType + ".up.sql", upFile)
+		if err != nil {
+			exitGracefully(err)
+		}
+
+		err = copyFileFromTemplate("templates/migrations/migration." + dbType + ".down.sql", downFile)
+		if err != nil {
+			exitGracefully(err)
+		}
 	}
 	return nil
 }
