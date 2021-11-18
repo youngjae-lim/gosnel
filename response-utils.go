9f1ba572ef8b64c1a27d2e2246eb097b324cad3e
Youngjae Lim <limyoungjae@gmail.com>
2021-11-17 22:59:49 -0500

    added Encrypt, Decrypt helper functions to helpers.go

--- response-utils.go
+++ response-utils.go
@@ -54,7 +54,7 @@ func (g *Gosnel) WriteXML(w http.ResponseWriter, status int, data interface{}, h
 func (g *Gosnel) DownloadFile(w http.ResponseWriter, r *http.Request, pathToFile, fileName string) error {
 	fp := path.Join(pathToFile, fileName)
 	fileToServe := filepath.Clean(fp)
-	w.Header().Set("Content-Type", fmt.Sprintf("attachment; file=\"%s\"", fileName))
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; file=\"%s\"", fileName))
 	http.ServeFile(w, r, fileToServe)
 	return nil
 }
