6110270a12e460bb020361b8926ed1fa79b8aa61
Youngjae Lim <limyoungjae@gmail.com>
2021-11-22 23:09:57 -0500

    got started adding a mail package to provide email service

---
+++ mailer/mail.go
@@ -0,0 +1,97 @@
+package mailer
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+)
+
+type Mail struct {
+	Domain      string
+	Templates   string
+	Host        string
+	Port        string
+	Username    string
+	Password    string
+	Encryption  string
+	FromAddress string
+	FromName    string
+	Jobs        chan Message
+	Results     chan Result
+	API         string
+	APIKey      string
+	APIUrl      string
+}
+
+type Message struct {
+	From        string
+	FromName    string
+	To          string
+	Subject     string
+	Template    string
+	Attachments []string
+	Data        interface{}
+}
+
+type Result struct {
+	Success bool
+	Error   error
+}
+
+func (m *Mail) ListenForMail() {
+	for {
+		msg := <-m.Jobs
+		err := m.Send(msg)
+		if err != nil {
+			m.Results <- Result{false, err}
+		} else {
+			m.Results <- Result{true, nil}
+		}
+	}
+}
+
+func (m *Mail) Send(msg Message) error {
+	// TODO: are we using an API or SMTP?
+	return m.SendSMTPMessage(msg)
+}
+
+func (m *Mail) SendSMTPMessage(msg Message) error {
+
+	return nil
+}
+
+func (m *Mail) buildHTMLMessage(msg Message) (string, error) {
+	templateToRender := fmt.Sprintf("%s/%s.html.tmpl", m.Templates, msg.Template)
+
+	t, err := template.New("email-html").ParseFiles(templateToRender)
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	if err = t.ExecuteTemplate(&tpl, "body", msg.Data); err != nil {
+		return "", err
+	}
+
+	formattedMessage := tpl.String()
+
+	return formattedMessage, nil
+}
+
+func (m *Mail) buildPlainTextMessage(msg Message) (string, error) {
+	templateToRender := fmt.Sprintf("%s/%s.text.tmpl", m.Templates, msg.Template)
+
+	t, err := template.New("email-text").ParseFiles(templateToRender)
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	if err = t.ExecuteTemplate(&tpl, "body", msg.Data); err != nil {
+		return "", err
+	}
+
+	plainMessage := tpl.String()
+
+	return plainMessage, nil
+}
