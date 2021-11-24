922b3d383be3f4d7500a7277cd6c10399306176b
Youngjae Lim <limyoungjae@gmail.com>
2021-11-23 21:25:02 -0500

    fixed the issue where sending email using api wasn't working

--- gosnel.go
+++ gosnel.go
@@ -122,10 +122,10 @@ func (g *Gosnel) New(rootPath string) error {
 
 	g.InfoLog = infoLog
 	g.ErrorLog = errorLog
-	g.Mail = g.createMailer()
 	g.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
 	g.Version = version
 	g.RootPath = rootPath
+	g.Mail = g.createMailer()
 	g.Routes = g.routes().(*chi.Mux)
 
 	g.config = config{
