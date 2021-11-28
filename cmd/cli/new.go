ab49c5b57a39b3b9f5d1ff69c9fe224c43698c98
Youngjae Lim <limyoungjae@gmail.com>
2021-11-27 22:26:03 -0500

    added codes for sanitizing application name

--- cmd/cli/new.go
+++ cmd/cli/new.go
@@ -1,12 +1,20 @@
 package main
 
-import "strings"
+import (
+	"log"
+	"strings"
+)
 
 func doNew(appName string) {
 	appName = strings.ToLower(appName)
 
 	// sanitize the application name (convert url to single word)
+	if strings.Contains(appName, "/") {
+		exploded := strings.SplitAfter(appName, "/")
+		appName = exploded[(len(exploded) - 1)]
+	}
 
+	log.Println("App name is", appName)
 
 	// git clone the skeleton application
 
@@ -27,6 +35,6 @@ func doNew(appName string) {
 
 
 	// run do mod tidy in the project directory
-	
+
 
 }
