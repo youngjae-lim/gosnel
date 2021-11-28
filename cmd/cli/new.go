0dc47cf128738e9317ca10c65cbcc3661aeda336
Youngjae Lim <limyoungjae@gmail.com>
2021-11-27 22:47:36 -0500

    added codes for cloning a remote repository right into Go

--- cmd/cli/new.go
+++ cmd/cli/new.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"log"
+	"os"
 	"strings"
+
+	"github.com/fatih/color"
+	"github.com/go-git/go-git/v5"
 )
 
 func doNew(appName string) {
@@ -17,24 +21,26 @@ func doNew(appName string) {
 	log.Println("App name is", appName)
 
 	// git clone the skeleton application
-
+	color.Green("\tCloning repository...")
+	_, err := git.PlainClone("./" + appName, false, &git.CloneOptions{
+		URL: "git@github.com/youngjae-lim/gosnel-app.git",
+		Progress: os.Stdout,
+		Depth: 1,
+	})
+	if err != nil {
+		exitGracefully(err)
+	}
 
 	// remove .git directory
 
-
 	// create a ready to go .env file
-
 
 	// create a makefile
 
-
 	// update the go.mod file
-
 
 	// update existing .go files with correct name/imports
 
-
 	// run do mod tidy in the project directory
-
 
 }
