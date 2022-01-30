17d243a5526d349eafa9ddf977647d276a9686d3
Youngjae Lim <limyoungjae@gmail.com>
2022-01-30 18:34:35 -0500

    Implemented List() for minio filesystem interface

--- filesystems/miniofilesystem/minio.go
+++ filesystems/miniofilesystem/minio.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"log"
 	"path"
+	"strings"
 
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/youngjae-lim/gosnel/filesystems"
 )
 
 type Minio struct {
@@ -50,4 +52,48 @@ func (m *Minio) Put(fileName, folder string) error {
 	}
 
 	return nil
+}
+
+func (m *Minio) List(prefix string) ([]filesystems.Listing, error) {
+	var listing []filesystems.Listing
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// get a minio client
+	client := m.getCredentials()
+
+	// get channels of ObjectInfo
+	objectCh := client.ListObjects(ctx, m.Bucket, minio.ListObjectsOptions{
+		Prefix:    prefix,
+		Recursive: true,
+	})
+
+	// loop through ObjectInfo channels
+	for object := range objectCh {
+		if object.Err != nil {
+			fmt.Println(object.Err)
+			return listing, object.Err
+		}
+
+		if !strings.HasPrefix(object.Key, ".") {
+			// convert file size(byte) to megabyte
+			b := float64(object.Size)
+			kb := b / 1024
+			mb := kb / 1024
+
+			// set Listing
+			item := filesystems.Listing{
+				Etag:         object.ETag,
+				LastModified: object.LastModified,
+				Key:          object.Key,
+				Size:         mb,
+			}
+
+			// append the item to a slice of Listing
+			listing = append(listing, item)
+		}
+	}
+
+	return listing, nil
 }
