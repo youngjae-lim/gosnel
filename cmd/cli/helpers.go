b09c9896160532629bf0301f836847ccdeb4eec1
Youngjae Lim <limyoungjae@gmail.com>
2022-02-07 10:48:27 -0500

    Fixed showHelp() text alignment issue

--- cmd/cli/helpers.go
+++ cmd/cli/helpers.go
@@ -78,19 +78,19 @@ func checkForDB() {
 func showHelp() {
 	color.Yellow(`Available commands:
 
-	help			- show the help commands
-  down      - put the server into maintenance mode
-  up        - take the server out of maintenance mode
-	version			- print application version
-	migrate			- runs all up migrations that have not been run previsouly
-	migrate down		- reverses the most recent migration
-	migrate reset		- runs all down migrations in reverse order, and then all up migrations
-	make auth		- creates and runs migrations for authentication tables, and creates models and middlewares
-	make migration <name> <format> 	- creates two new up and down migrations in the /migrations folder; format=sql/fizz (default fizz)
-	make handler <name>	- creates a stub handler in the /handlers directory
-	make model <name> 	- creates a new model in the /data directory
-	make session 		- creates a table in the database as a session store
-	make mail <name>    	- creates two starter mail templates in the mail directory
+help			- show the help commands
+down        - put the server into maintenance mode
+up          - take the server out of maintenance mode
+version			- print application version
+migrate			- runs all up migrations that have not been run previsouly
+migrate down		- reverses the most recent migration
+migrate reset		- runs all down migrations in reverse order, and then all up migrations
+make auth		- creates and runs migrations for authentication tables, and creates models and middlewares
+make migration <name> <format> 	- creates two new up and down migrations in the /migrations folder; format=sql/fizz (default fizz)
+make handler <name>	  - creates a stub handler in the /handlers directory
+make model <name> 	  - creates a new model in the /data directory
+make session 		  - creates a table in the database as a session store
+make mail <name>    	  - creates two starter mail templates in the mail directory
 
 	`)
 }
